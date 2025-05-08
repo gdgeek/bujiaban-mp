9dc90e3590dcb5663f6a631a7a78791a2b126d78
gdgeek <dirui1@yeah.net>
2025-05-08 16:40:18 +0800

    no message

---
+++ src/pages/checkin/index.vue
@@ -0,0 +1,154 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { onLoad } from "@dcloudio/uni-app";
+const openid = ref<string | null>(null);
+const token = ref<string | null>(null);
+const status = ref<string | null>(null);
+const getStatus = async (id: string) => {
+  return new Promise((resolve, reject) => {
+    console.error("refresh:");
+    wx.request({
+      url: "https://w.4mr.cn/v1/checkin/status?openid=" + id,
+      method: "GET",
+      success: function (res) {
+        console.log("登录成功！" + JSON.stringify(res));
+        resolve(res.data);
+      },
+      fail: function (res) {
+        console.log("请求失败！" + res.errMsg);
+        reject(res.errMsg);
+      },
+    });
+  });
+};
+//得到 openid
+const getQueryString = (url: string, name: string): string | null => {
+  var reg = new RegExp("(^|&|/?)" + name + "=([^&|/?]*)(&|/?|$)", "i");
+  var r = url.substring(1).match(reg);
+  if (r != null) {
+    return r[2];
+  }
+  return null;
+};
+
+const refresh = async () => {
+  const ready = await getStatus(openid.value);
+  status.value = ready.data.status;
+};
+const login = async () => {
+  return new Promise((resolve, reject) => {
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          wx.request({
+            url: "https://w.4mr.cn/v1/we-chat/openid",
+            data: {
+              code: res.code,
+            },
+            method: "POST",
+            success: function (res) {
+              //console.log("openid:" + res.data.openid);
+              // openid.value = res.data.openid;
+              //  console.error("openid!!!!:" + res.data);
+              resolve(res.data);
+            },
+            fail: function (res) {
+              console.log("请求失败！" + res.errMsg);
+              reject(res.errMsg);
+            },
+          });
+        } else {
+          console.log("登录失败！" + res.errMsg);
+          reject(res.errMsg);
+        }
+      },
+      fail: function (res) {
+        console.log("登录失败！" + res.errMsg);
+        reject(res.errMsg);
+      },
+    });
+  });
+};
+const begin = async () => {
+  await ready();
+  await refresh();
+};
+const stop = async () => {
+  await close();
+  await refresh();
+};
+const ready = async () => {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: "https://w.4mr.cn/v1/checkin/ready",
+      method: "POST",
+      data: {
+        openid: openid.value,
+        token: token.value,
+      },
+      success: function (res) {
+        console.log("请求成功！" + JSON.stringify(res));
+        resolve(res.data);
+      },
+      fail: function (res) {
+        console.log("请求失败！" + res.errMsg);
+        reject(res.errMsg);
+      },
+    });
+  });
+};
+const close = () => {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: "https://w.4mr.cn/v1/checkin/close?openid=" + openid.value,
+      method: "DELETE",
+      success: function (res) {
+        console.log("删除成功！！！" + JSON.stringify(res));
+        resolve(res.data);
+      },
+      fail: function (res) {
+        console.log("请求失败！" + res.errMsg);
+        reject(res.errMsg);
+      },
+    });
+  });
+};
+onLoad(async () => {
+  try {
+    const ret = await login();
+
+    console.log("openid:" + ret.openid);
+    openid.value = ret.openid;
+    await refresh();
+    //console.error("ready!:" + JSON.stringify(ready.data.status));
+
+    const query = getCurrentPages()[getCurrentPages().length - 1].options;
+
+    const decodedUrl = decodeURIComponent(query.q);
+    token.value = getQueryString(decodedUrl, "k");
+    if (!token.value) {
+      token.value = "test123";
+    }
+    ///openid.value = openidRes.data.openid;
+  } catch (error) {
+    console.log("请求失败！" + error);
+  }
+});
+</script>
+<template>
+  <view class="index">
+    <p>openid:{{ openid }}</p>
+    <p>token:{{ token }}</p>
+    <p>status:{{ status }}</p>
+    <view v-if="status == 'waiting'">
+      <button @click="begin">准备开始</button>
+    </view>
+    <view v-if="status == 'ready'">
+      <button @click="stop">放弃录制</button>
+    </view>
+  </view>
+</template>
+
+<style lang="scss">
+//
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-//
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <view class="index">home</view>
+  <view class="index">home!</view>
 </template>
 
 <style lang="scss">
