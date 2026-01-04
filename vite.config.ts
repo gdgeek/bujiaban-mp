5c81b615ffdc0c58fd2d506386821495d3c28191
gdgeek <dirui1@yeah.net>
2026-01-04 15:11:07 +0800

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,12 @@ export default defineConfig({
     // 开发阶段启用源码映射：https://uniapp.dcloud.net.cn/tutorial/migration-to-vue3.html#需主动开启-sourcemap
     sourcemap: process.env.NODE_ENV === "development",
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler", // 使用新版 Sass API，消除弃用警告
+      },
+    },
+  },
   plugins: [uni()],
 });
